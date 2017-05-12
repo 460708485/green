47adaf402e27c2eeed2f617472f31c6a8d33f932
460708485 <460708485@qq.com>
2017-05-12 18:02:28 +0800

    asddddddd

--- src/main/java/com/wang/green/common/BaseController.java
+++ src/main/java/com/wang/green/common/BaseController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * 基础控制类 异常统一处理
@@ -32,13 +34,22 @@ public class BaseController {
 	}
 
 	public void handleException(ResponseVO<Void> vo, Throwable t) {
-
 		
+		vo.addError(CommonErrorEnum.COMMON_ERROR_1);
+		if(logger.isErrorEnabled()){
+			String requestURI = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getRequestURI();
+			logger.error("系统处理错误："+requestURI , t);
+		}
 		
 	}
 
 	private void handleException(ResponseVO<Void> vo, CommonException e) {
-
+			if(null != e.getErrorEnum()){
+				vo.addError(e.getErrorEnum());
+			}
+		
+		
+		
 	}
 
 }
