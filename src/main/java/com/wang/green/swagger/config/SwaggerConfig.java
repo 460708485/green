18f89b9779b6bf8eb208428c221744e5703e4497
460708485 <460708485@qq.com>
2017-05-18 21:08:40 +0800

    添加swagger（restful接口API文档）

---
+++ src/main/java/com/wang/green/swagger/config/SwaggerConfig.java
@@ -0,0 +1,62 @@
+package com.wang.green.swagger.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+
+/**
+ * @author wangjq
+ *
+ */
+//@Configuration
+@EnableSwagger
+//@EnableWebMvc
+public class SwaggerConfig {
+
+    private SpringSwaggerConfig springSwaggerConfig; 
+    
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    /**
+     * Required to autowire SpringSwaggerConfig
+     */
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig)
+    {
+    	springSwaggerConfig.jacksonSwaggerSupport();
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    /**
+     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+     * framework - allowing for multiple swagger groups i.e. same code base
+     * multiple swagger resource listings.
+     */
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation()
+    {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiInfo(apiInfo())
+                .includePatterns(".*");
+    }
+
+    private ApiInfo apiInfo()
+    {
+    	
+    	requestMappingHandlerAdapter.getMessageConverters();
+        ApiInfo apiInfo = new ApiInfo(
+                "绿蔬管理系统",
+                "绿蔬管理系统-所有接口。",
+                "开发者: ",
+                "wangjq",
+                "Green  License",
+                "/GREENLICENSE");
+        return apiInfo;
+    }
+}
